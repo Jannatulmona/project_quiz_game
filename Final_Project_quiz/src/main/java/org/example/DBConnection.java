d7741df57d32e7ddc95ee77c6aaddaad1e9547c5
Jannatul Mawa Mona <it23029@mbstu.ac.bd>
2025-12-10 15:19:08 +0600

    Add files via upload

---
+++ Final_Project_quiz/src/main/java/org/example/DBConnection.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/quiz_app";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Poddopatarjol30348@,"; // আপনার MySQL password
+
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
